8d48d7594cbc885c04403815e4383f3c34541f8e
manojkumarm <117139060+manojkumarm-git@users.noreply.github.com>
2023-04-17 08:40:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,6 @@ terraform {
 
   required_providers {
   
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.56.0"
-    }
     artifactory = {
 
       source = "jfrog/artifactory"
@@ -25,18 +21,15 @@ terraform {
 
 }
 
-
-# Configure the Artifactory provider
-
 provider "artifactory" {
 
-  url           = "https://sandeepmkp.jfrog.io/artifactory"
+  url           = "https://clouddsein.jfrog.io/artifactory"
 
-  access_token  = "eyJ2ZXIiOiIyIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYiLCJraWQiOiJEUjBFNTdnRHVycXpNZFR2TVJfajYwRGhVbXkyc3JYWGYwZ2tyVXhUSkhjIn0.eyJzdWIiOiJqZmFjQDAxZnM0NTF3NDJ5OHBnMTlydGF5cmEwNGN0XC91c2Vyc1wvbWFub2oiLCJzY3AiOiJhcHBsaWVkLXBlcm1pc3Npb25zXC9hZG1pbiIsImF1ZCI6WyJqZnJ0QCoiLCJqZmFjQCoiLCJqZmV2dEAqIiwiamZtZEAqIiwiamZjb25AKiJdLCJpc3MiOiJqZmZlQDAxZnM0NTF3NDJ5OHBnMTlydGF5cmEwNGN0IiwiZXhwIjoxNjgyNzUxMDI4LCJpYXQiOjE2ODAxNTkwMjgsImp0aSI6Ijc0YjhlZjExLTU5YjctNDA4Mi1iY2Y5LTNlOWQzZjBjNzY5YyJ9.VPt_aWLdKBj0_DBWpmUVOmt0VZxFap4yighSo44TJzxuSOFN52GR5uPTsh7v1neTcv31tLMDS5HVMwwejie1dA9uMAxlREA-leNBzuPyr5A6zrCqfOdq-SX1XK2yzLqQXX-40Jwu10dsJo959MN3xIcWhDK7t4Hybb-1J2TIgS5Qni7F9ZKV4CzZjx4xMcg0iSMeVTRcxpM4hoXQ0SzO1GpCvTroixiajUK_T6xGbQ0EcJiyECRySpuZctSnE9HI6XhKzZoRUHnb_vrCl1nbI9TF5SmNOUfNTXEAY5BmYPI2boIoTwn0TnFqR3uEH1PZQE6D_zV19a83n_9M_KJCqg"
-
+  access_token  = "eyJ2ZXIiOiIyIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYiLCJraWQiOiJEeFBFMzAzVUd2alNWaV80VUZpV0tZRFR2TldocGJ1OXhZR2VTMHA2MmVzIn0.eyJzdWIiOiJqZmFjQDAxZ3R3MTZqMDFtdmozMTV0eWh4YTYxczI4XC91c2Vyc1wvbWFub2oiLCJzY3AiOiJhcHBsaWVkLXBlcm1pc3Npb25zXC9hZG1pbiIsImF1ZCI6IipAKiIsImlzcyI6ImpmZmVAMDFndHcxNmowMW12ajMxNXR5aHhhNjFzMjgiLCJpYXQiOjE2ODE2OTk4NzUsImp0aSI6ImMzZTllZmJiLTMwOWQtNDcwNC1hNjNkLTNmNmQ4ZTIzMmYzZSJ9.gg3lZVXdrfhAwOolWg4xXyUCOkCDSvLlUBD3drK2lmK81l0-Zd_AgljdgDA1xyKle8JUrzfZ8qDvva8mTW9FokLGhROP9q1qjgIbJ7k4nk0pMW-04poh4HGCUoswnRhkAMhOkTtljFDXEaqTtZHRD3l_PGjgDirciTQd_GIxktwmBV6RS_jeie0pi-uZoncFQVIxPuzyt5LoJ8tPtIfpUS38gqSXFNRG7UC0vpiTlIyTsuuWdQc71phLZx33vQkTAeCfHwmGOLjJ69CfgrLSHcy31J-47XS6Lfxm0oOEEww43t5AlpdUjj2b_fjm-nGcoHWBd4XmOr9Wh-Z0EMu8sA"
 }
 
-module "aft" {
-  source  = "sandeepmkp.jfrog.io/test-terraform__aws-ia/control_tower_account_factory/aws"
+module "mymodule" {
+
+    source  = "clouddsein.jfrog.io/mk-terraform__myns/mymodule/myprovider"
 
 }
