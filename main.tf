6108e0db349dca5d1ce7109a3c2ede561fc2ec9a
manojkumarm <117139060+manojkumarm-git@users.noreply.github.com>
2023-06-30 11:13:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,24 +22,23 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 provider "artifactory" {
-  url           = "https://hts2.jfrog.io/artifactory"
-  access_token  = "eyJ2ZXIiOiIyIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYiLCJraWQiOiJ5ek11SE53Ukhfei0yTmJ1UWpwZVhXdFRqemJyWnVGcUtJRldvZXdYQW1VIn0.eyJzdWIiOiJqZnJ0QDAxZThoa3Q2NTU1eTc0MDI2Z2I3MzYxMWhhL3VzZXJzL21hbm9qIiwic2NwIjoiYXBwbGllZC1wZXJtaXNzaW9ucy91c2VyIiwiYXVkIjoiamZydEAwMWU4aGt0NjU1NXk3NDAyNmdiNzM2MTFoYSIsImlzcyI6ImpmcnRAMDFlOGhrdDY1NTV5NzQwMjZnYjczNjExaGEvdXNlcnMvX3N5c3RlbV8iLCJleHAiOjE2ODc5NDM5MzgsImlhdCI6MTY4NTM1MTkzOCwianRpIjoiMzE5NThhMDEtOTNhOC00NWY5LWE1MjMtYjc1MjE4N2YwNTY0In0.VjQ1QnOZv5TIB4Wr2f6lJyGNdYwvUJfVPLJfWw6Pqdoau5oIZKkc2VibGtP2JWHhvx00abtprkDHff41r1HDBHFz8yrY9as_54KjbA-ZcdZ-OACLcsXu41DkPJmE8O7GT5pdkfkjGiqZo_D7HLUdeVIVGjkrzIDmjKPfpvGop0ewhNYz3tp2dFf0XAbJFEN4MDiHDgXJtuWfo5q-mWq_8cfFs12JSWLtxRbRFvkzTaZpdQw0UZdoW-ZuyuZemuevfOKtqudAZfrHmD5moPEDqC0qfH2ixAyJ3rVz90znnZ4nU5Hqrz96hQoQGHBvBwe_f7q4UawQZ9SaKBLYvgfgJQ"
-}
+  url           = "https://test.jfrog.io/artifactory"
+  access_token  = "xxxxxxxx"
 module "aft" {
-  source  = "hts2.jfrog.io/mk-terraform__aws-ia/control_tower_account_factory/aws"
+  source  = "test.jfrog.io/mk-terraform__aws-ia/control_tower_account_factory/aws"
 
   # Required Parameters
-  ct_management_account_id    = "211478285720"
-  log_archive_account_id      = "316241107683"
-  audit_account_id            = "017855766036"
-  aft_management_account_id   = "008782125710"
+  ct_management_account_id    = "xxxx"
+  log_archive_account_id      = "xxx"
+  audit_account_id            = "xxx"
+  aft_management_account_id   = "xxxx"
   ct_home_region              = "us-west-2"
   tf_backend_secondary_region = "us-east-1"
 
   # Optional Parameters
   terraform_distribution = "tfc"
   vcs_provider           = "codecommit"
-  terraform_org_name     = "mutualofenumclaw"
+  terraform_org_name     = "xxx"
   terraform_token        = "removed"
 
   # Optional Feature Flags
